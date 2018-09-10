0b92a4caecd03007db46de3184176b77558c21df
Marcus Schimizzi <marcjschimizzi@gmail.com>
2018-09-10 03:05:49 -0400

    few more responsiveness fixes yet

--- src/components/Subcomponent.vue
+++ src/components/Subcomponent.vue
@@ -1,28 +1,41 @@
 <template lang="html">
 
   <section class="subcomponent">
-    <template v-if="orientation === 'left'">
+    <div class="desktop">
+      <template v-if="orientation === 'left'">
+        <div class="third float">
+          <h2>{{title}}</h2>
+        </div>
+        <div class="third float">
+          <p>{{text}}</p>
+        </div>
+        <div class="third float">
+          <img :src="image">
+        </div>
+      </template>
+      <template v-else-if="orientation === 'right'">
+        <div class="third float">
+          <img :src="image">
+        </div>
+        <div class="third float">
+          <p>{{text}}</p>
+        </div>
+        <div class="third float">
+          <h2>{{title}}</h2>
+        </div>
+      </template>
+    </div>
+    <div class="mobile">
       <div class="third float">
         <h2>{{title}}</h2>
       </div>
       <div class="third float">
-        <p>{{text}}</p>
-      </div>
-      <div class="third float">
-        <img :src="image">
-      </div>
-    </template>
-    <template v-else-if="orientation === 'right'">
-      <div class="third float">
         <img :src="image">
       </div>
       <div class="third float">
         <p>{{text}}</p>
       </div>
-      <div class="third float">
-        <h2>{{title}}</h2>
-      </div>
-    </template>
+    </div>
   </section>
 
 </template>
@@ -57,5 +70,19 @@ export default class Sub extends Vue {
   h2 {
     font-weight: bold;
     font-size: 48px;
+    @media screen and (max-width: 600px) {
+      font-size: 36px;
+    }
+  }
+  .mobile {
+    display: none;
+    @media screen and (max-width: 600px) {
+      display: block;
+    }
+  }
+  .desktop {
+    @media screen and (max-width: 600px) {
+      display: none;
+    }
   }
 </style>
